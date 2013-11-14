76d40f604fa7dbcaea2f4f799b895a54ddfedcb1
pthorin <peter.thorin@kartena.se>
2013-11-14 14:59:32 +0100

    Generated by generator-bespoke

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,21 @@ bespoke.horizontal.from('article', {
   hash: true,
   bullets: 'li, .bullet'
 });
+
+var map = L.TileJSON.createMap('map', lmvTileJson);
+map.setView([57.70062008114879, 11.97602928666158], 13);
+
+function pulseIcon(r, className) {
+  if (r == null) r = 50;
+  var d = r * 2;
+  return L.divIcon({
+    html: '<div class="pulse"></div><div class="point'+
+      (className?' '+className:'')+'"></div>',
+    className: 'map-icon',
+    iconSize: L.point(d, d),
+    iconAnchor: L.point(r, r)
+  });
+}
+
+var _pulseIcon = pulseIcon();
+L.marker([57.70106857948243, 11.975991878576991], { icon: _pulseIcon}).addTo(map);
